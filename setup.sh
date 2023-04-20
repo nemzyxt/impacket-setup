6fcaf2bcafe51a7fe1d85db04bd9eed56e694eec
Nemuel Wainaina <bR0S3@protonmail.com>
2023-04-20 14:08:33 +0300

    Add the setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+sudo git clone https://github.com/SecureAuthCorp/impacket.git /opt/impacket
+sudo pip3 install -r /opt/impacket/requirements.txt
+cd /opt/impacket/ 
+sudo pip3 install .
+sudo python3 setup.py install
